1255fd3b20eaefcc36cc8c5aa7e8bce7a4ec2871
student <student@student-System-Product-Name>
2016-07-28 19:28:23 +0300

    Token generation

--- LaksmiCareWatch/js/ui-controller.js
+++ LaksmiCareWatch/js/ui-controller.js
@@ -1,5 +1,5 @@
 var pagePos = 0;
-var pageCount = 3;
+var pageCount = 2;
 window.guid = null;
 window.user = null;
 var d1 = document.getElementById("heart-rate");
@@ -9,10 +9,7 @@ var counter = 0;
 
 
 $(document).ready(function() {
-//	genDeviceId();
-	window.guid = generateGUID();
-	console.log(window.guid);
-	alert(window.guid);
+    genDeviceId();
     document.addEventListener("rotarydetent", function(e) {
         if (e.detail.direction == "CW") {
             $("#page" + pagePos).removeClass("ui-page-active");
@@ -24,17 +21,6 @@ $(document).ready(function() {
             $("#page" + pagePos).addClass("ui-page-active");
         }
     }, false);
-   /* if (window.guid)
-        $("#device-id-label").val(window.guid);
-    else
-        window.guid = generateGUID();
-    findUserByToken(window.guid, function(data) {
-        window.user = data;
-        alert("User registered!");
-    }, function() {
-    	alert("Device not registered at system");
-    });
-*/
 });
 
 function genDeviceId(){
@@ -44,27 +30,25 @@ function genDeviceId(){
 	    window.alert("OOPS! Token storage is unavailable!");
 	    tizen.application.getCurrentApplication().exit();
 	}
-	var req = window.indexedDB.open("tokens", 10);
+	var req = window.indexedDB.open("tokens", 1);
 	req.onupgradeneeded = function(event){
 		var db = event.target.result;
-		var tokenStore = db.createObjectStore("tokens", { keyPath: "id" });
+		var tokenStore = db.createObjectStore("tokens", { keyPath: "name" });
 		tokenStore.createIndex("guid", "guid", { unique: false });
 	};
 	req.onsuccess = function(event){
 		var db = event.target.result;
 		var tokens = db.transaction(["tokens"], "readwrite").objectStore("tokens");
-		req = tokens.get("1");
+		req = tokens.get("default");
 		req.onsuccess = function(event){
 			if(req.result)
 					console.log(req.result.guid);
-			else
-				tokens.put({id : "1", guid : generateGUID()});
-			alert("Device id registered!");
+			else{
+				window.guid = {name: "default", guid : generateGUID()};
+				tokens.put(window.guid);
+			}
+			alert("Device id registered! Device id = " + window.guid);
 		};
-		/*req.onerror = function(event){
-			tokens.add({"id" : "1", "guid" : generateGUID()});
-			alert("Device id not registered!");
-		};*/
 	};
 	req.onerror = function(ev){
 		alert("Can not open db");
@@ -72,7 +56,32 @@ function genDeviceId(){
 	
 }
 function resetDeviceId() {
-	
+	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+	if (!window.indexedDB) {
+	    window.alert("OOPS! Token storage is unavailable!");
+	    tizen.application.getCurrentApplication().exit();
+	}
+	var req = window.indexedDB.open("tokens", 1);
+	req.onsuccess = function(event){
+		var db = event.target.result;
+		var tokens = db.transaction(["tokens"], "readwrite").objectStore("tokens");
+		req = tokens.get("default");
+		req.onsuccess = function(event){
+			if(req.result){
+					console.log(req.result.guid);
+					window.guid = {name: "default", guid : generateGUID()};
+					tokens.put(window.guid);
+					alert("Device id changed!");
+			}
+			else{
+				alert("Device id not changed!");
+			}
+		};
+	};
+	req.onerror = function(ev){
+		alert("Can not open db");
+	};
 }
 
 function startHeartMonitor() {
@@ -85,60 +94,46 @@ function onchangedCB(hrmInfo) {
     counter++;
     if (counter > 10 && hrmInfo.heartRate > 0) {
         tizen.humanactivitymonitor.stop("HRM");
-        console.log("heart");
-        console.log("geo");
-        var alarm = {
-            heartRate: 70,  
-            measureDate: "21 January",
-            gpsPosition: {
-                latitude: 45,
-                logitude: 56
-            }
-        };
-        
-        sendAlarm(window.guid, alarm, function() {
-         	alert("Alarm sended! Wait for help!");
-         }, function() {
-         	alert("Can not send alarm!");
-         });
-        navigator.geolocation.getCurrentPosition(function(loc) {
-        	console.log("geo");
-            var alarm = {
-                heartRate: 70,
-                measureDate: "21 January",
-                gpsPosition: {
-                    latitude: loc.coords.latitude,
-                    logitude: location.coords.longitude
+        counter = 0;
+        window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+    	if (!window.indexedDB) {
+    	    window.alert("OOPS! Token storage is unavailable!");
+    	    tizen.application.getCurrentApplication().exit();
+    	}
+    	var req = window.indexedDB.open("tokens", 1);
+    	req.onsuccess = function(event){
+    		var db = event.target.result;
+    		var tokens = db.transaction(["tokens"], "readwrite").objectStore("tokens");
+    		req = tokens.get("default");
+    		console.log(req);
+    		req.onsuccess = function(ev){
+    			console.log(ev.result);
+    			window.guid = req.result.guid;
+        		console.log("Send alarm + " + guid);
+        		if(navigator.geolocation){
+                	navigator.geolocation.getCurrentPosition(function(position) {
+                    	var alarm = {
+                                heartRate: hrmInfo.heartRate,  
+                                measureDate: (new Date()).toString(),
+                                gpsPosition: {
+                                    latitude: position.coords.latitude,
+                                    logitude: position.coords.longitude
+                                }
+                        };
+                        
+                        sendAlarm(window.guid, alarm, function() {
+                         	alert("Alarm sended! Wait for help!");
+                         }, function() {
+                         	alert("Can not send alarm!");
+                         });
+                	}, function(error){
+                		console.log("Geolocation error " + error);
+                	});
                 }
-            };
-            
-            sendAlarm(window.guid, alarm, function() {
-             	alert("Alarm sended! Wait for help!");
-             }, function() {
-             	alert("Can not send alarm!");
-             });
-    	
-            /*window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-        	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-        	if (!window.indexedDB) {
-        	    window.alert("OOPS! Token storage is unavailable!");
-        	    tizen.application.getCurrentApplication().exit();
-        	}
-        	var tokens = window.indexedDB.transaction(["tokens"]).objectStore("tokens");
-        	var req = tokens.get("1");
-        	req.onsuccess = function(event){
-        		sendAlarm(req.result.guid, alarm, function() {
-                 	alert("Alarm sended! Wait for help!");
-                 }, function() {
-                 	alert("Can not send alarm!");
-                 });
-        	}
-            sendAlarm(window.guid, alarm, function() {
-             	alert("Alarm sended! Wait for help!");
-             }, function() {
-             	alert("Can not send alarm!");
-             });*/
-        });
+    		};
+    	};
+    
             
     }
 }
